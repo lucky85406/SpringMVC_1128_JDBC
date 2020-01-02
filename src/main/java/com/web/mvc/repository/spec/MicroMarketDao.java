1ffbea7c83255ac68a378496c19ac66176fdf53f
study <study@MB-207-14>
2020-01-02 19:28:34 +0800

    jdbc

---
+++ src/main/java/com/web/mvc/repository/spec/MicroMarketDao.java
@@ -0,0 +1,9 @@
+package com.web.mvc.repository.spec;
+
+import com.web.mvc.entity.MicroMarket;
+import java.util.List;
+
+
+public interface MicroMarketDao {
+    List<MicroMarket> queryMicroMarket();
+}
